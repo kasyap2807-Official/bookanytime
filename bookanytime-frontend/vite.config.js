1084473d45859347d3bc2b434fc27c7a7c7dfb91
kasyap2807-Official <backup.kasyap@gmail.com>
2025-08-10 14:15:38 +0530

    Update vite.config.js

--- bookanytime-frontend/vite.config.js
+++ bookanytime-frontend/vite.config.js
@@ -8,6 +8,6 @@ server: {
     host: "0.0.0.0", // Allow access from external devices
     port: 5173, // Change if needed
     strictPort: true, // Ensures the exact port is used
-    allowedHosts: ["ec2-204-236-252-142.compute-1.amazonaws.com"], // ✅ Add your domain here
+    allowedHosts: ["ec2-50-16-122-218.compute-1.amazonaws.com"], // ✅ Add your domain here
   },
 })
